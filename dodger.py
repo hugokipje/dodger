cd2a0453b44df3f431745af176ed7d771ea55a92
LucasNagtegaal <45482502+LucasNagtegaal@users.noreply.github.com>
2021-04-02 11:57:48 +0200

    Update dodger.py
    basis gelegd voor controls met meerdere mogelijkheden om te besturen (pijltjes + wasd)

--- dodger.py
+++ dodger.py
@@ -72,7 +72,43 @@ while True:
     reverseCheat = slowCheat = False
     slechterik_toevoegen = 0
     pygame.mixer.music.play(-1, 0.0)
-    
-    #alleen als je de game aan het spelen bent
+
     while True: 
         score += 1 
+
+# if event.type == KEYDOWN:
+#                 if event.key == ord('z'):
+#                     reverseCheat = True
+#                 if event.key == ord('x'):
+#                     slowCheat = True
+#                 if event.key == K_LEFT or event.key == ord('a'):
+#                     moveRight = False
+#                     moveLeft = True
+#                 if event.key == K_RIGHT or event.key == ord('d'):
+#                     moveLeft = False
+#                     moveRight = True
+#                 if event.key == K_UP or event.key == ord('w'):
+#                     moveDown = False
+#                     moveUp = True
+#                 if event.key == K_DOWN or event.key == ord('s'):
+#                     moveUp = False
+#                     moveDown = True
+
+#             if event.type == KEYUP:
+#                 if event.key == ord('z'):
+#                     reverseCheat = False
+#                     score = 0
+#                 if event.key == ord('x'):
+#                     slowCheat = False
+#                     score = 0
+#                 if event.key == K_ESCAPE:
+#                         eindigen()
+
+#                 if event.key == K_LEFT or event.key == ord('a'):
+#                     moveLeft = False
+#                 if event.key == K_RIGHT or event.key == ord('d'):
+#                     moveRight = False
+#                 if event.key == K_UP or event.key == ord('w'):
+#                     moveUp = False
+#                 if event.key == K_DOWN or event.key == ord('s'):
+#                     moveDown = False
