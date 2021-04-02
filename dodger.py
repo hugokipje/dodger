ed73a6e7d4e87652d9a146b56550ced5a9c6c291
LucasNagtegaal <45482502+LucasNagtegaal@users.noreply.github.com>
2021-04-02 13:51:24 +0200

    Update dodger.py
    goeierik volgt nu de muis en je kan ook met wasd en pijltjes besturen

--- dodger.py
+++ dodger.py
@@ -76,39 +76,47 @@ while True:
     while True: 
         score += 1 
 
-# if event.type == KEYDOWN:
-#                 if event.key == ord('z'):
-#                     reverseCheat = True
-#                 if event.key == ord('x'):
-#                     slowCheat = True
-#                 if event.key == K_LEFT or event.key == ord('a'):
-#                     moveRight = False
-#                     moveLeft = True
-#                 if event.key == K_RIGHT or event.key == ord('d'):
-#                     moveLeft = False
-#                     moveRight = True
-#                 if event.key == K_UP or event.key == ord('w'):
-#                     moveDown = False
-#                     moveUp = True
-#                 if event.key == K_DOWN or event.key == ord('s'):
-#                     moveUp = False
-#                     moveDown = True
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                eindigen()
 
-#             if event.type == KEYUP:
-#                 if event.key == ord('z'):
-#                     reverseCheat = False
-#                     score = 0
-#                 if event.key == ord('x'):
-#                     slowCheat = False
-#                     score = 0
-#                 if event.key == K_ESCAPE:
-#                         eindigen()
+            if event.type == KEYDOWN:
+                if event.key == ord('z'):
+                    reverseCheat = True
+                if event.key == ord('x'):
+                    slowCheat = True
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == ord('w'):
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveUp = False
+                    moveDown = True
 
-#                 if event.key == K_LEFT or event.key == ord('a'):
-#                     moveLeft = False
-#                 if event.key == K_RIGHT or event.key == ord('d'):
-#                     moveRight = False
-#                 if event.key == K_UP or event.key == ord('w'):
-#                     moveUp = False
-#                 if event.key == K_DOWN or event.key == ord('s'):
-#                     moveDown = False
+            if event.type == KEYUP:
+                if event.key == ord('z'):
+                    reverseCheat = False
+                    score = 0
+                if event.key == ord('x'):
+                    slowCheat = False
+                    score = 0
+                if event.key == K_ESCAPE:
+                        eindigen()
+
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = False
+
+            if event.type == MOUSEMOTION:
+                # If the mouse moves, move the player where the cursor is.
+                plaatje_raakvlak.move_ip(event.pos[0] - plaatje_raakvlak.centerx, event.pos[1] - plaatje_raakvlak.centery)
